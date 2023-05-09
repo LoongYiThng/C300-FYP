b49b18d6083528f822613074f0a59ec299dab0c5
LoongYiThng <loong.yithng@gmail.com>
2023-05-09 19:11:42 +0800

    more mobile friendliness

--- sanitizerHome.php
+++ sanitizerHome.php
@@ -10,21 +10,21 @@
         <h1>choose how you want to get started</h1>
 
         <div class="container" style="padding: 50px 0px 50px">
-            <div class="row">
-                <div class="col">
-                    <div class="card">
+            <div class="row gy-3">
+                <div class="col-md-4">
+                    <div class="card h-150">
                         <h4><b>upload a microsoft word file (.docx)</b></h4>
                         <p>suitable for large files</p>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
+                <div class="col-md-4">
+                    <div class="card h-150">
                             <h4><b>upload a text file (.txt)</b></h4>
                             <p>suitable for small files</p>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
+                <div class="col-md-4">
+                    <div class="card h-150">
                         <h4><b>paste raw text</b></h4>
                         <p>fast and suitable for testing different sanitization techniques or extremely small strings of text</p>
                     </div>
