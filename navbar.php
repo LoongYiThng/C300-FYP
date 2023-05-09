5e88b882ddf9494733094e08441b789fc98349dc
LoongYiThng <loong.yithng@gmail.com>
2023-05-09 18:23:26 +0800

    css, cards, and navbar reformatted

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <!-- navigation bar -->
-<nav class="navbar navbar-expand-sm bg-light navbar-light">
-                    
-    <a class="navbar-brand" href="sanitizerHome.php"><i class="fas fa-home"></i>document sanitizer</a>
+<nav class="navbar navbar-custom navbar-expand-sm bg-dark navbar-dark fixed-top">             
+    
+    <a class="navbar-brand" href="sanitizerHome.php">Document Sanitizer <i class="fas fa-file"></i></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,10 +9,10 @@
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="searchTechniques.php"><i class="fas fa-search"></i>Search Techniques</a>
+                <a class="nav-link" href="searchTechniques.php">Search Techniques <i class="fas fa-search"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="about.php"><i class="fas fa-phone-alt"></i>About</a>
+                <a class="nav-link" href="about.php">About <i class="fas fa-question-circle"></i></a>
             </li>
         </ul>
     </div>
--- sanitizerHome.php
+++ sanitizerHome.php
@@ -7,5 +7,30 @@
     </head>
     <body>
         <?php include "navbar.php"; ?>
+        <h1>choose how you want to get started</h1>
+
+        <div class="container" style="padding: 50px 0px 50px">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <h4><b>upload a microsoft word file (.docx)</b></h4>
+                        <p>suitable for large files</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="container">
+                            <h4><b>upload a text file (.txt)</b></h4>
+                            <p>suitable for small files</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <h4><b>paste raw text</b></h4>
+                        <p>fast and suitable for testing different sanitization techniques or extremely small strings of text</p>
+                    </div>
+                </div>
+            </div>
     </body>
 </html>
