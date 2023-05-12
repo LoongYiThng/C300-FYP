58bfd3768a5e69997f89e04a0d10f2a153d794b7
LoongYiThng <loong.yithng@gmail.com>
2023-05-12 09:30:16 +0800

    swapped  order of inline and block elements

--- about.php
+++ about.php
@@ -8,11 +8,11 @@
     </head>
     <body>
         <?php include "navbar.php"; ?>
-        <div>
+
+        <div class="margin">
             <h1>Purpose</h1>
-            <h2><p style="text-align: center;">To automate data sanitizing through the use of scripts or web application</p></h2>
+            <p style="text-align: center;"><h2>To automate data sanitizing through the use of scripts or web application</h2></p>
         </div>
-        
 
         <h1>How it works</h1>
         <h1>To find out more</h1>
