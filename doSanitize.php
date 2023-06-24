61848aae2b09a9f0580ae58f7d8f9795d1089597
LoongYiThng <loong.yithng@gmail.com>
2023-06-24 16:21:20 +0800

    added input validation

--- doSanitize.php
+++ doSanitize.php
@@ -8,6 +8,39 @@
     <body>
         <?php include "navbar.php"; ?>
 
-        <h1>this is the sanitization page</h1>
+        <?php
+        $files=array("wordFile", "textFile", "pasteText", "pdfFile", "excelFile");
+        $formatSelected=false;
+        foreach ($files as $value) {
+            if (!empty($_GET[$value])) {
+                $formatSelected=true;
+                $retrievedFilename=$_GET[$value];
+            }
+        }
+        if ($formatSelected==false) {
+            echo "error: the website has neither detected a file submitted or a file uploaded previously. <br>";
+        }
+
+        $techniques=array("characterMasking", "syntheticData", "dataPerturbation", "recordSurpression",
+        "generalisation", "pseudonymisation", "swapping", "attributeSurpression");
+        $techniqueSelected=false;
+        foreach ($techniques as $value) {
+            if (isset($_GET[$value])) {
+                $techniqueSelected=true;
+            }
+        }
+        if ($techniqueSelected==false) {
+            echo "error: it appears you have not made any selection on which technique you intend to use.";
+        }
+
+        #if all goes well
+        if ($formatSelected and $techniqueSelected) {
+            echo "pass <br>";
+            print("filename: ".$retrievedFilename);  
+        }
+        ?>
+
+        <h1>WIP hashes</h1>
+        <h2>for now these are the hashes of the uploaded files, not the downloaded files<h2>
     </body>
 </html>
